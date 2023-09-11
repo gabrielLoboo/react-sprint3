491ec396549c38c42e59dc91050f05a1882b0715
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2023-09-11 12:16:23 -0300

    component Main criado

--- react-app/src/components/Main/Main.jsx
+++ react-app/src/components/Main/Main.jsx
@@ -10,6 +10,17 @@ export default function Main() {
                 src={background}
             />
             <img/>
+
+            <p className="text-wrapper-12">Tranquilidade garantida com nosso processo de seguro simples e eficiente:</p>
+          <img
+            className="portoholding-RGB-h-2"
+            alt="Portoholding RGB h"
+            src="https://cdn.animaapp.com/projects/64fef685b6e2a0e2da10edd7/releases/64fef7818f2f44c908882012/img/portoholding-rgb-h-2-1@2x.png"
+          />
+          <p className="text-wrapper-13">Proteção que acompanha seu ritmo sobre duas rodas!</p>
+          <div className={"button-cadastre-se"}>
+            <div className="text-wrapper">Cadastre-se</div>
+            </div>
         </div>
     </>
   )
