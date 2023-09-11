3d6e6aedeee5f586fb8f81fb9a08cb0a47798d55
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2023-09-11 19:14:00 -0300

    componente Navbar finalizado

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Main from './components/Main/Main'
 
 import Navbar from './components/Navbar/Navbar'
 
@@ -7,6 +8,7 @@ function App() {
     <>
 
       <Navbar />
+      <Main />
     </>
   )
 }
--- react-app/src/components/Navbar/Navbar.jsx
+++ react-app/src/components/Navbar/Navbar.jsx
@@ -5,19 +5,13 @@ import sIcon from "../../assets/searchIcon.png"
 export default function Navbar() {
   return (
         <div className="navbar">
-            <img
-                className="image"
-                alt="Image"
-                src={bikeLogo}
-            />
+            <img className="image" alt="Image" src={bikeLogo} />
+           
             <a href="" className="div">Home</a>
             <a href="" className="div">Serviços</a>
             <a href="" className="text-wrapper-2">Nossos Integrantes</a>
-            <img
-                className="search-icon"
-                alt="Search Icon"
-                src={sIcon}
-            />
+            
+            <img className="search-icon" alt="Search Icon" src={sIcon} />
         </div>
   )
 }
