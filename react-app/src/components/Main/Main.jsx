533767a413f409e453b6fe4408251cbde5463b72
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2023-09-11 18:07:25 -0300

    Background e textos adicionados

--- react-app/src/components/Main/Main.jsx
+++ react-app/src/components/Main/Main.jsx
@@ -1,27 +1,31 @@
-import background from '../../assets/background.png'
+import background from '../../assets/background.png';
+import portoLogo from '../../assets/portoLogo.png';
+
+import './style.css';
 
 export default function Main() {
+  const backgroundStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    width: '100%',
+    height: '100vh',
+    
+  };
+
   return (
     <>
-        <div className="index">
-            <img
-                className="background"
-                alt="Background"
-                src={background}
-            />
-            <img/>
+      <div className="index" style={backgroundStyle}>
+        
+        <p className="text-wrapper-13">Proteção que acompanha seu ritmo sobre duas rodas!</p>
 
-            <p className="text-wrapper-12">Tranquilidade garantida com nosso processo de seguro simples e eficiente:</p>
-          <img
-            className="portoholding-RGB-h-2"
-            alt="Portoholding RGB h"
-            src="https://cdn.animaapp.com/projects/64fef685b6e2a0e2da10edd7/releases/64fef7818f2f44c908882012/img/portoholding-rgb-h-2-1@2x.png"
-          />
-          <p className="text-wrapper-13">Proteção que acompanha seu ritmo sobre duas rodas!</p>
-          <div className={"button-cadastre-se"}>
-            <div className="text-wrapper">Cadastre-se</div>
-            </div>
+        <img className="portoholding-RGB-h-2" alt="Logo Porto" src={portoLogo} />
+
+        <div className="button-cadastre-se">
+          <div className="text-wrapper">Cadastre-se</div>
         </div>
+        
+      </div>
     </>
-  )
+  );
 }
