21ad5951ad9d264d5afee87e837e1a4dcd0ca660
jixatos <j.gody2004@gmail.com>
2023-09-12 20:51:47 -0300

    Correção o integrantes.jsx

--- react-app/src/routes/Integrantes/Integrantes.jsx
+++ react-app/src/routes/Integrantes/Integrantes.jsx
@@ -5,11 +5,11 @@ export default function Integrantes() {
 
   return (
     <ul className='listIntegrantes'>
-        <li>Gabriel Francisco Lobo | RM99708</li>
-        <li>Juan Godoy | RM551408</li>
+        <li>Gabriel Francisco Lobo | RM99708 | 1TDSPF</li>
+        <li>Juan Godoy | RM551408 | 1TDSPF</li>
         <li>Kayque Lima Nunes | RM550782</li>
         <li>Henrique Bernardon Kaawi Massucato | RM99993</li>
-        <li>Ivan Rangel Pestana Marcolin | RM551005</li>
+        <li>Ivan Rangel Pestana Marcolin | RM551005 | 1TDSPF</li>
 
         <a href="https://github.com/gabrielLoboo/react-sprint3">Repositório Github</a>
     </ul>
